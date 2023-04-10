94ebe1665ffb93a2c2bb107cbeb867e00ef5d442
Mohammed Hisham Moizuddin <78191578+hishammoizuddin@users.noreply.github.com>
2023-04-10 12:23:09 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 // This cpp file calls the searchEngine function
 // Author : Mohammed Hisham Moizuddin
-// CS 251 - Project 2 - Search Engine
+
 #include <iostream>
 #include "search.h"
 #include <sstream>
@@ -11,15 +11,5 @@ int main() {
     string filename;
     getline(cin, filename, '\n');
     searchEngine(filename);
-// map <string, set<string>> myData;
-// int x = buildIndex("tiny.txt", myData);
-
-// for (auto e : myData) {
-//     cout<<e.first<<" -> ";
-//     for (auto d: e.second) {
-//         cout<<d<<", ";
-//     }
-//     cout<<endl;
-// }
     return 0;
 }
